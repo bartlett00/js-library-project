6541fabbf77037ebf67c652d188b784d8f4d2366
Pax <113464089+bartlett00@users.noreply.github.com>
2024-06-04 12:06:06 -0500

    Build form to add books to display.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read == true ? 'read' : 'not read yet'}`;
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read == 'true' ? 'have read' : 'not read yet'}`;
     }
 };
 
@@ -25,6 +25,35 @@ function displayBooks() {
             cardContainer.appendChild(bookCard);
     });
 }
+
+function displayBook(book) {
+    let bookCard = document.createElement('div');
+    bookCard.setAttribute('class', 'card');
+    bookCard.innerText = book.info();
+    cardContainer.appendChild(bookCard);
+}
+
+
+let bookForm = document.getElementById('add-book');
+
+bookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let newTitle = document.getElementById('title').value;
+    let newAuthor = document.getElementById('author').value;
+    let newPageNum = document.getElementById('page-num').value;
+    let newRead = bookForm.elements['read'].value;
+
+    bookFormSidebar(newTitle, newAuthor, newPageNum, newRead);
+});
+
+function bookFormSidebar(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    addBookToLibrary(book);
+    displayBook(book);
+}
+
+
 
 let dune = new Book('Dune', 'Frank Herbert', '896', true);
 addBookToLibrary(dune);
