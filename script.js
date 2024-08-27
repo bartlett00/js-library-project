1ab6b1e068c6dbc3316f26e1a49d14ff530ad722
Pax <113464089+bartlett00@users.noreply.github.com>
2024-08-27 10:50:50 -0500

    Add constraint validation to book form, require title, author, and page count.

--- script.js
+++ script.js
@@ -1,85 +1,108 @@
 let myLibrary = [];
-const cardContainer = document.querySelector('#card-container');
+const cardContainer = document.querySelector("#card-container");
 
 class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-        this.info = function() {
-            return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read == 'true' ? 'have read' : 'not read yet'}`;
-        }
-    }
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+      return `${this.title} by ${this.author}, ${this.pages} pages, ${
+        this.read == "true" ? "have read" : "not read yet"
+      }`;
+    };
+  }
 
-    addBookToLibrary = () => {
-        myLibrary.push(this);
-    }
+  addBookToLibrary = () => {
+    myLibrary.push(this);
+  };
 
-    displayBook = () => {
-        let bookCard = document.createElement('div');
-        let removeBtn = document.createElement('button');
-        let readStatusBtn = document.createElement('button');
+  displayBook = () => {
+    let bookCard = document.createElement("div");
+    let removeBtn = document.createElement("button");
+    let readStatusBtn = document.createElement("button");
 
-        bookCard.setAttribute('class', 'card');
-        bookCard.setAttribute('data-index', myLibrary.indexOf(this));
-        bookCard.innerText = this.info();    
+    bookCard.setAttribute("class", "card");
+    bookCard.setAttribute("data-index", myLibrary.indexOf(this));
+    bookCard.innerText = this.info();
 
-        readStatusBtn.setAttribute('class', 'readStatusBtn');
-        readStatusBtn.innerText = 'Toggle Read';
-        readStatusBtn.addEventListener('click', (e) => {
-            if(this.read == 'true') {
-                this.read = 'false';
-            } else {
-                this.read = 'true';
-            }
-            bookCard.textContent = this.info();
-            bookCard.appendChild(removeBtn);
-            bookCard.appendChild(readStatusBtn);
-        });
+    readStatusBtn.setAttribute("class", "readStatusBtn");
+    readStatusBtn.innerText = "Toggle Read";
+    readStatusBtn.addEventListener("click", (e) => {
+      if (this.read == "true") {
+        this.read = "false";
+      } else {
+        this.read = "true";
+      }
+      bookCard.textContent = this.info();
+      bookCard.appendChild(removeBtn);
+      bookCard.appendChild(readStatusBtn);
+    });
 
-        removeBtn.setAttribute('class', 'removeBtn');
-        removeBtn.innerText = 'Remove';
-        removeBtn.addEventListener('click', (e) => {
-            cardContainer.removeChild(bookCard);
-            let newLibraryArr = myLibrary.filter((entry) => {
-                return entry !== myLibrary[myLibrary.indexOf(this)];
-            });
-            myLibrary = newLibraryArr;
-        });
+    removeBtn.setAttribute("class", "removeBtn");
+    removeBtn.innerText = "Remove";
+    removeBtn.addEventListener("click", (e) => {
+      cardContainer.removeChild(bookCard);
+      let newLibraryArr = myLibrary.filter((entry) => {
+        return entry !== myLibrary[myLibrary.indexOf(this)];
+      });
+      myLibrary = newLibraryArr;
+    });
 
-
-        cardContainer.appendChild(bookCard);
-        bookCard.appendChild(removeBtn);
-        bookCard.appendChild(readStatusBtn);
-    }
+    cardContainer.appendChild(bookCard);
+    bookCard.appendChild(removeBtn);
+    bookCard.appendChild(readStatusBtn);
+  };
 }
 
-let bookForm = document.getElementById('add-book');
+let bookForm = document.getElementById("add-book");
 
-bookForm.addEventListener('submit', (e) => {
+let newTitle = document.getElementById("title");
+let newAuthor = document.getElementById("author");
+let newPageNum = document.getElementById("page-num");
+let newRead = bookForm.elements["read"];
+// let readBooleanFS = document.querySelector("#read-or-not");
+
+bookForm.addEventListener("submit", (e) => {
+  if (newTitle.validity.valueMissing) {
+    newTitle.setCustomValidity("A book title is required");
+    newTitle.reportValidity();
     e.preventDefault();
-
-    let newTitle = document.getElementById('title').value;
-    let newAuthor = document.getElementById('author').value;
-    let newPageNum = document.getElementById('page-num').value;
-    let newRead = bookForm.elements['read'].value;
-
-    addBookCard(newTitle, newAuthor, newPageNum, newRead);
+  } else if (newAuthor.validity.valueMissing) {
+    newAuthor.setCustomValidity("An author is required.");
+    newAuthor.reportValidity();
+    e.preventDefault();
+  } else if (newPageNum.validity.valueMissing) {
+    newPageNum.setCustomValidity("A page number is required.");
+    newPageNum.reportValidity();
+    e.preventDefault();
+    // } else if (readBooleanFS.validity.valueMissing) {
+    //   readBooleanFS.setCustomValidity("Please select an option.");
+    //   readBooleanFS.reportValidity();
+    //   e.preventDefault();
+  } else {
+    addBookCard(
+      newTitle.value,
+      newAuthor.value,
+      newPageNum.value,
+      newRead.value
+    );
+  }
 });
 
 function addBookCard(title, author, pages, read) {
-    let book = new Book(title, author, pages, read);
-    book.addBookToLibrary();
-    book.displayBook();
+  let book = new Book(title, author, pages, read);
+  book.addBookToLibrary();
+  book.displayBook();
 }
 
-let sidebarBtn = document.querySelector('#new-book');
+let sidebarBtn = document.querySelector("#new-book");
 
-sidebarBtn.addEventListener('click', (e) => {
-    bookForm.classList.toggle('hidden');
+sidebarBtn.addEventListener("click", (e) => {
+  bookForm.classList.toggle("hidden");
 });
 
-let dune = new Book('Dune', 'Frank Herbert', '896', 'true');
+let dune = new Book("Dune", "Frank Herbert", "896", "true");
 dune.addBookToLibrary();
 dune.displayBook();
